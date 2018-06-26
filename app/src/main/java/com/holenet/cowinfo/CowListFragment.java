a4789b8405e49620972f26ea657a39705da8643f
holenet <holenet6147@naver.com>
2018-06-26 20:11:53 +0900

    insert CowRecyclerAdapter to the CowListFragment

--- app/src/main/java/com/holenet/cowinfo/CowListFragment.java
+++ app/src/main/java/com/holenet/cowinfo/CowListFragment.java
@@ -150,75 +150,75 @@ public class CowListFragment extends Fragment {
             existErrors(errors, getHolder().getContext());
         }
     }
-}
 
-class CowRecyclerAdapter extends RecyclerView.Adapter<CowRecyclerAdapter.ViewHolder> {
-    private final List<Cow> cows;
-    private final OnCowSelectedListener listener;
+    static class CowRecyclerAdapter extends RecyclerView.Adapter<CowRecyclerAdapter.ViewHolder> {
+        private final List<Cow> cows;
+        private final OnCowSelectedListener listener;
 
-    public CowRecyclerAdapter(List<Cow> items, OnCowSelectedListener listener) {
-        cows = items;
-        this.listener = listener;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_cow, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.cow = cows.get(position);
-        char sexSymbol = (holder.cow.sex.equals("female") ? '♀' : '♂');
-        String summary = holder.cow.number.split("-")[2]+" "+sexSymbol;
-        holder.tVsummary.setText(summary);
-        holder.tVcount.setText("이력 "+String.valueOf(holder.cow.records.size()));
-        holder.tVnumber.setText(cows.get(position).number);
-        if (holder.cow.birthday != null) {
-            holder.tVbirthday.setVisibility(View.VISIBLE);
-            holder.tVbirthday.setText("출생 "+holder.cow.birthday);
-        } else {
-            holder.tVbirthday.setVisibility(View.GONE);
-        }
-
-        holder.view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (listener != null) {
-                    listener.onCowSelected(holder.cow, holder.getAdapterPosition());
-                }
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return cows.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View view;
-        final TextView tVsummary, tVcount, tVnumber, tVbirthday;
-        Cow cow;
-
-        ViewHolder(View view) {
-            super(view);
-            this.view = view;
-            tVsummary = view.findViewById(R.id.tVsummary);
-            tVcount = view.findViewById(R.id.tVcount);
-            tVnumber = view.findViewById(R.id.tVnumber);
-            tVbirthday = view.findViewById(R.id.tVbirthday);
+        public CowRecyclerAdapter(List<Cow> items, OnCowSelectedListener listener) {
+            cows = items;
+            this.listener = listener;
         }
 
         @Override
-        public String toString() {
-            return super.toString() + " '" + tVnumber.getText() + "'";
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_cow, parent, false);
+            return new ViewHolder(view);
         }
-    }
 
-    public interface OnCowSelectedListener {
-        void onCowSelected(Cow cow, int position);
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.cow = cows.get(position);
+            char sexSymbol = (holder.cow.sex.equals("female") ? '♀' : '♂');
+            String summary = holder.cow.number.split("-")[2]+" "+sexSymbol;
+            holder.tVsummary.setText(summary);
+            holder.tVcount.setText("이력 "+String.valueOf(holder.cow.records.size()));
+            holder.tVnumber.setText(cows.get(position).number);
+            if (holder.cow.birthday != null) {
+                holder.tVbirthday.setVisibility(View.VISIBLE);
+                holder.tVbirthday.setText("출생 "+holder.cow.birthday);
+            } else {
+                holder.tVbirthday.setVisibility(View.GONE);
+            }
+
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onCowSelected(holder.cow, holder.getAdapterPosition());
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return cows.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View view;
+            final TextView tVsummary, tVcount, tVnumber, tVbirthday;
+            Cow cow;
+
+            ViewHolder(View view) {
+                super(view);
+                this.view = view;
+                tVsummary = view.findViewById(R.id.tVsummary);
+                tVcount = view.findViewById(R.id.tVcount);
+                tVnumber = view.findViewById(R.id.tVnumber);
+                tVbirthday = view.findViewById(R.id.tVbirthday);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + tVnumber.getText() + "'";
+            }
+        }
+
+        public interface OnCowSelectedListener {
+            void onCowSelected(Cow cow, int position);
+        }
     }
 }
